81229ba0c23f8ade6f826d9a72578aa3b59f142a
Melissa Grant <grantmelissa92@gmail.com>
2025-11-30 23:50:11 -0500

    Edited information display

--- world.php
+++ world.php
@@ -15,8 +15,24 @@ $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
+    </tr>
+  </thead> 
+  <tbody>
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?= $row['name']; ?></td>
+        <td><?= $row['continent']; ?></td>
+        <td><?= $row['independence_year']; ?></td>
+        <td><?= $row['head_of_state']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
